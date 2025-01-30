75a88b3a978570c4ed8db5e0797c71638507cc9d
Christopher Anaya <chrisanaya32109@gmail.com>
2025-01-30 10:01:19 -0700

    Playing around

--- Program.cs
+++ Program.cs
@@ -1,104 +1,11 @@
-﻿int examAssignments = 5;
+﻿// Code sample 1
+bool flag = true;
+int value = 10;
 
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
-
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-
-int[] studentScores = new int[10];
-
-string currentStudentLetterGrade = "";
-
-Console.Clear();
-Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
-
-foreach (string name in studentNames)
+if (flag)
 {
-    string currentStudent = name;
-
-    if (currentStudent == "Sophia")
-        studentScores = sophiaScores;
-
-    else if (currentStudent == "Andrew")
-        studentScores = andrewScores;
-
-    else if (currentStudent == "Emma")
-        studentScores = emmaScores;
-
-    else if (currentStudent == "Logan")
-        studentScores = loganScores;
-
-    int sumAssignmentScores = 0;
-
-    int sumExtraCredit = 0;
-
-    decimal currentStudentGrade = 0;
-
-    int gradedAssignments = 0;
-
-    foreach (int score in studentScores)
-    {
-        gradedAssignments += 1;
-
-        if (gradedAssignments <= examAssignments)
-            sumAssignmentScores += score;
-
-        else
-            sumExtraCredit += score;
-            // sumAssignmentScores += score / 10;
-    }
-
-    currentStudentGrade = ((decimal)(sumAssignmentScores) + (decimal)(sumExtraCredit) / 10) / examAssignments;
-
-    decimal noExtraCreditGrade = (decimal)(sumAssignmentScores) / examAssignments;
-
-    decimal extraCredit = (decimal)(sumExtraCredit) / (gradedAssignments - examAssignments);
-
-    decimal gainedPoints = (decimal)currentStudentGrade - (decimal)noExtraCreditGrade;
-
-    if (currentStudentGrade >= 97)
-        currentStudentLetterGrade = "A+";
-
-    else if (currentStudentGrade >= 93)
-        currentStudentLetterGrade = "A";
-
-    else if (currentStudentGrade >= 90)
-        currentStudentLetterGrade = "A-";
-
-    else if (currentStudentGrade >= 87)
-        currentStudentLetterGrade = "B+";
-
-    else if (currentStudentGrade >= 83)
-        currentStudentLetterGrade = "B";
-
-    else if (currentStudentGrade >= 80)
-        currentStudentLetterGrade = "B-";
-
-    else if (currentStudentGrade >= 77)
-        currentStudentLetterGrade = "C+";
-
-    else if (currentStudentGrade >= 73)
-        currentStudentLetterGrade = "C";
-
-    else if (currentStudentGrade >= 70)
-        currentStudentLetterGrade = "C-";
-
-    else if (currentStudentGrade >= 67)
-        currentStudentLetterGrade = "D+";
-
-    else if (currentStudentGrade >= 63)
-        currentStudentLetterGrade = "D";
-
-    else if (currentStudentGrade >= 60)
-        currentStudentLetterGrade = "D-";
-
-    else
-        currentStudentLetterGrade = "F";
-    
-    Console.WriteLine($"{currentStudent}\t\t{noExtraCreditGrade}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{extraCredit} ({gainedPoints} pts)");
+    value += 10;
+    Console.WriteLine($"Inside the code block: {value}");
 }
 
-Console.WriteLine("\n\rPress the Enter key to continue");
-Console.ReadLine();
+Console.WriteLine($"Outside the code block: {value}");
